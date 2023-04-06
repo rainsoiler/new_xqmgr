835fcec8d4e43fa2a7eab057cbcd699d4e2383e7
luyanan <luyanan0718@163.com>
2023-04-06 18:24:32 +0800

    修改启动类的位置

--- rainsoil-web/src/main/java/com/github/rainsoil/fastapi/web/AppApplication.java
+++ rainsoil-web/src/main/java/com/github/rainsoil/fastapi/web/AppApplication.java
@@ -1,4 +1,4 @@
-package com.github.rainsoil.fastapi.web.system;
+package com.github.rainsoil.fastapi.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
